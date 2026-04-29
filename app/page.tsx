8015eb7eed18dfb220e68ef50400205ca65e7032
omid <omid@omids-MacBook-Air.local>
2026-04-28 23:03:26 -0400

    disable cache

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,5 @@
+export const dynamic = "force-dynamic";
+export const revalidate = 0;
 import Link from "next/link";
 import { PlusCircle, Download } from "lucide-react";
 import { createServerSupabase } from "@/lib/supabase-server";
